800a26fc391271838bd34b65dfac564150bf758e
zhkos <desutadesuta@gmail.com>
2023-06-27 23:28:51 +0300

    shlypa

--- apt-collecting-packages/packages.sh
+++ apt-collecting-packages/packages.sh
@@ -4,14 +4,13 @@ fetch-packages() {
   apt-get download $(apt-cache depends --recurse \
     --no-recommends --no-suggests --no-conflicts --no-breaks \
     --no-replaces --no-enhances "$PCKG" |
-    grep "^\w" | grep -v "$(dpkg --print-architecture)" | sort -u) >/dev/null &&
-    echo "$LOG_SUCCESS Packages downloaded"
+    grep "^\w" | grep -v "$(dpkg --print-architecture)" | sort -u) >/dev/null 2>&1 &&
+    echo "$LOG_SUCCESS [ $PCKG ] Downloaded"
 }
 
 scan-pack() {
-  dpkg-scanpackages -m -t deb "$PATH_TO_PCKGS" | gzip -9c >"$PATH_TO_PCKGS"Packages.gz 2>/dev/null &&
-    dpkg-scanpackages -m -t deb "$PATH_TO_PCKGS" >"$PATH_TO_PCKGS"Release 2>/dev/null
-  echo "$LOG_SUCCESS Output Packages.gz and Release files created"
+  dpkg-scanpackages -m -t deb "$PATH_TO_PCKGS" | gzip -9c >"$PATH_TO_PCKGS"Packages.gz
+  echo "$LOG_SUCCESS Output Packages.gz created"
 }
 
 update-cache() {
@@ -21,18 +20,18 @@ update-cache() {
 
 filter-packages() {
   for PCKG in $TARGET_PACKAGES; do
-    echo "$LOG_INFO Try to find package: $PCKG"
+    #echo "$LOG_INFO [ $PCKG ] : Try to find package"
     [[ "$(apt-cache policy "$PCKG")" == *"$PCKG"* ]]
     pckg_exist="$?"
     [[ "$(apt-cache policy "$PCKG" | grep -Eo 'Candidate:.*')" != "Candidate: (none)" ]]
     pckg_none="$?"
     if [[ "$pckg_exist" == 0 && "$pckg_none" == 0 ]]; then
-      echo "$LOG_SUCCESS Package $PCKG found"
+      echo "$LOG_SUCCESS [ $PCKG ] : Package found"
       PCKGNAME_DIR_NAME="${PCKGNAME_DIR_NAME}_$PCKG"
       PCKG_FILTERED_LIST="${PCKG_FILTERED_LIST} $PCKG"
       ((i += 1))
     else
-      echo "$LOG_FAILURE Package $PCKG not found"
+      echo "$LOG_FAILURE [ $PCKG ] : Package not found"
       continue
     fi
   done
@@ -46,17 +45,17 @@ creating-dirs() {
 }
 
 function main {
-  LOG_INFO="[Info]:"
-  LOG_SUCCESS="[Success]:"
-  LOG_FAILURE="[Failure]:"
+  LOG_INFO="[I]:"
+  LOG_SUCCESS="[S]:"
+  LOG_FAILURE="[F]:"
 
   echo "$LOG_INFO Input packages: $TARGET_PACKAGES"
   update-cache
   filter-packages
-  echo "$LOG_SUCCESS Found packages: $PCKG_FILTERED_LIST"
+  echo "$LOG_SUCCESS [ $PCKG_FILTERED_LIST ] Found packages"
 
   for PCKG in $PCKG_FILTERED_LIST; do
-    echo "$LOG_INFO Download packages for: $PCKG"
+    #echo "$LOG_INFO [ $PCKG ] Download package"
     fetch-packages
   done
 
